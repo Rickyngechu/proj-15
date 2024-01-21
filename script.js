45dd8a679ed4b181fa92bd16a77680aa6d143b43
Rickyngechu <ngerick00@gmail.com>
2024-01-21 19:54:48 +0300

    Navigation

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 "use strict";
 const humb = document.querySelector(".hamburger");
 const mobileCont = document.querySelector(".mb-ct");
+const mobileNav = document.querySelector(".mobile-nav");
+
 humb.addEventListener("click", function () {
   const src = this.getAttribute("src");
   console.log("You have clicked me!");
   console.log(src);
   humb.src = "/images/icon-close.svg";
+
+  mobileNav.classList.toggle("hide-nav-mobile");
 });
